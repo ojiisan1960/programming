fa979a7f473a2db170161b1fbf25a15964c8ce21
Mark <mark.wogahn@gmail.com>
2018-09-27 21:40:41 -0400

    Work up to chap 3 step 26

--- C/build_an_editor/kilo.c
+++ C/build_an_editor/kilo.c
@@ -2,10 +2,10 @@
  * This is an instruction booklet that shows you how to build a text editor in C.
  *
  * https://viewsourcecode.org/snaptoken/kilo/index.html
- * Compeleted
+ * Completed
  *  Chap 1: Setup
  *  Chap 2: Entering Raw Mode
- *
+ * 	Chap 3: rawInputAndOutput ( fail to build at Step 26 )
  */
 
 /*** includes ***/
@@ -16,23 +16,34 @@
 #include <unistd.h>
 #include <termios.h>
 
-/*** data ***/
+/** DEFINES ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/*** DATA ***/
+
 struct termios orig_termios;
 
-/*** terminal ***/
+
+/*** TERMINAL ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
   perror(s);
   exit(1);
 }
 
 void disableRawMode() {
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) 
+	die("tcsetattr");
 }
 
 void enableRawMode() {
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+	if (tcsetattr(STDIN_FILENO, &orig_termios) == -1) 
+	die("tcgetattr");
 	atexit(disableRawMode);
+
 	struct termios raw = orig_termios;
 /* disable Ctrl-m, Ctrl-S and Ctrl-Q and misc*/
 	raw.c_iflag &= ~( BRKINT | ICRNL | INPCK | ISTRIP | IXON);
@@ -46,23 +57,56 @@ void enableRawMode() {
 	raw.c_cc[VMIN] = 0;
 	raw.c_cc[VTIME] = 1;
 
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) 
+	die("tcsetattr");
 }
 
-/*** main ***/
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+/*** OUTPUT ***/
+
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < 24; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
+void editorRefreshScreen() {
+/* clear the screen and position the cursor */
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  editorDrawRows();
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/*** INPUT ***/
+
+void editorProcessKeypress() {
+  char c = editorReadKey();
+  switch (c) {
+    case CTRL_KEY('q'):
+  		write(STDOUT_FILENO, "\x1b[2J", 4);
+  		write(STDOUT_FILENO, "\x1b[H", 3);
+      exit(0);
+      break;
+  }
+}
+/*** MAIN ***/
 
 int main(){
 	enableRawMode();
 
 	while (1) {
-	char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-		if (iscntrl(c)) {
-			printf("%d\r\n", c);
-		} else {
-			printf("%d ('%c')\r\n", c, c);
-		}
-		if ( c == 'q') break;
+		editorRefreshScreen();
+		editorProcessKeypress();
 	}
 	return 0;
 	
