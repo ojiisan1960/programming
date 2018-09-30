233f28589888be0cac044df66202065c52e5ba81
Mark <mark.wogahn@gmail.com>
2018-09-30 18:34:19 -0400

    finished up to chapt 3 step 31

--- C/build_an_editor/kilo.c
+++ C/build_an_editor/kilo.c
@@ -13,27 +13,40 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 #include <termios.h>
 
-/*** data ***/
-struct termios orig_termios;
+/*** Defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
 
-/*** terminal ***/
+/*** Data ***/
+struct editorConfig {
+	int screenrows;
+	int screencols;
+	struct termios orig_termios;
+};
+
+struct editorConfig E;
+
+/*** Terminal ***/
 
 void die(const char *s) {
-  perror(s);
-  exit(1);
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+  	perror(s);
+  	exit(1);
 }
 
 void disableRawMode() {
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) die("tcsetattr");
 }
 
 void enableRawMode() {
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
 	atexit(disableRawMode);
-	struct termios raw = orig_termios;
+	struct termios raw = E.orig_termios;
 /* disable Ctrl-m, Ctrl-S and Ctrl-Q and misc*/
 	raw.c_iflag &= ~( BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 /* disable newline and carriage return */
@@ -49,21 +62,97 @@ void enableRawMode() {
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-/*** main ***/
+char editorReadKey() {
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+	if (nread == -1 && errno != EAGAIN) die("read");
+  }
+	return c;
+}
 
-int main(){
-	enableRawMode();
-
-	while (1) {
-	char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+int getCursorPosition(int *rows, int *cols){
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+	
+	printf("\r\n");
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1) {
 		if (iscntrl(c)) {
 			printf("%d\r\n", c);
 		} else {
 			printf("%d ('%c')\r\n", c, c);
+			}
 		}
-		if ( c == 'q') break;
+
+	editorReadKey();
+	return -1;
+}
+
+int getWindowSize(int *rows, int *cols){
+	struct winsize ws;
+	
+	if ( 1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+		return getCursorPosition(rows, cols);
+	} else {
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
 	}
-	return 0;
+}
+
+/******************************************************************************/
+/*** Output ***/
+/******************************************************************************/
+
+void editorDrawRows(){
+	int y;
+	for (y = 0 ;y < E.screenrows; y++) {
+		write(STDOUT_FILENO, "~\r\n", 3);
+	}
+}
+
+void editorRefreshScreen(){
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+	editorDrawRows();
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+		
+}
+
+/******************************************************************************/
+/*** Input ***/
+/******************************************************************************/
+
+void editorProcessKeypress() {
+	char c = editorReadKey();
+  	switch (c) {
+    case CTRL_KEY('q'):
+	editorRefreshScreen();
+/*		write(STDOUT_FILENO, "\x1b[2J", 4);
+		write(STDOUT_FILENO, "\x1b[H", 3); 
+*/
+      exit(0);
+      break;
+  }
+}
+
+/*** Init ***/
+
+void initEditor() {
+	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+	}
+
+int main(){
+	enableRawMode();
+	initEditor();
+
+	while (1) {
+	editorRefreshScreen();
+	editorProcessKeypress();
+	}
+
+return 0;
 	
 }
---
+++ C/build_an_editor/kilo_2.c
@@ -0,0 +1,69 @@
+/*
+ * This is an instruction booklet that shows you how to build a text editor in C.
+ *
+ * https://viewsourcecode.org/snaptoken/kilo/index.html
+ * Compeleted
+ *  Chap 1: Setup
+ *  Chap 2: Entering Raw Mode
+ *
+ */
+
+/*** includes ***/
+#include <errno.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+
+/*** data ***/
+struct termios orig_termios;
+
+/*** terminal ***/
+
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
+void disableRawMode() {
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+}
+
+void enableRawMode() {
+	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+	atexit(disableRawMode);
+	struct termios raw = orig_termios;
+/* disable Ctrl-m, Ctrl-S and Ctrl-Q and misc*/
+	raw.c_iflag &= ~( BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+/* disable newline and carriage return */
+	raw.c_oflag &= ~( OPOST );
+/* set character size to 8 bits */
+	raw.c_cflag |= (CS8);
+/* disable echo, cononical mode, Ctrl-v, Ctrl-c, Ctrl-z */
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+/* read timeout */
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+/*** main ***/
+
+int main(){
+	enableRawMode();
+
+	while (1) {
+	char c = '\0';
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+		if (iscntrl(c)) {
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n", c, c);
+		}
+		if ( c == 'q') break;
+	}
+	return 0;
+	
+}
